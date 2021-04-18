fe47d45acbc585cd81fff14438fb8279e63685f3
HasanAlameh <hfalameh@umich.edu>
2021-04-18 14:18:21 -0400

    Initial commit. Changes to UI

---
+++ TreasuryDataParser_UI.py
@@ -0,0 +1,71 @@
+from tkinter import filedialog
+from tkinter import *
+import tkinter.font as font
+from parseTest import *
+import subprocess, os, platform
+
+ftypes = [
+    ('PDF', '*.pdf')
+]
+
+def browse_button():
+    # Allow user to select a directory and store it in global var
+    # called folder_path
+    filename = filedialog.askopenfile(filetypes=ftypes)
+    lbl1.config(text=filename.name)
+def program_instructions():
+    print("=============================USER INSTRUCTIONS================================================")
+    print("-  CLOSE THE MASTER CSV FILE PRIOR TO RUNNING THE PROGRAM, program cannot write to an open CSV file.")
+    print("-  The program can ONLY handle readable PDF files (not image PDFs, textual only), therefore \nplease check before you feed in the files to the system.")
+    print("-  Browse Button - This is used to navigate to your PDF file & get the path.")
+    print("-  Submit Button - This is used to initiate the parser.")
+    print("-  Open Button - This is used to open the CSV file after the data has been parsed and transferred into it.")
+    print("-  NOTE: The CSV file is rewritten everytime the tool is ran, \nwhich means the old data is wiped out and new data is written based on the PDF uploaded.\nTherefore, it's best to save the CSV elsewhere before trying a second run.")
+    print("==============================================================================================")
+def run_prog():
+    # This is your function maitra use the folder_path variable to get filename
+    saveCSVfilepath = file_parse(file_path)
+    finish_msg = StringVar()
+    lbl2 = Label(master = root, textvariable=finish_msg, bg='#FED000', fg='black', font = font.Font(size=15), height= 3, highlightthickness = 0, bd = 0)
+    finish_msg.set("MESSAGE: Your master CSV file is ready, \nClick on 'Open Master CSV' to open it! \n PATH: " + saveCSVfilepath)
+    lbl2.pack()
+
+def open_file():
+    filepath = 'parseRESULT.csv'
+    if platform.system() == 'Darwin':       # macOS
+        subprocess.call(('open', filepath))
+    elif platform.system() == 'Windows':    # Windows
+        os.startfile(filepath)
+    else:                                   # linux variants
+        subprocess.call(('xdg-open', filepath))
+
+
+root = Tk()
+program_instructions()
+myFont = font.Font(size=30)
+root.geometry("820x350")
+root.configure(bg='#1F1F1F')
+root.title("TREASURY DATA PARSER")
+
+frame1 = Frame(bg = '#1F1F1F', height=1)
+frame1.pack(fill=X)
+
+lbl1 = Label(frame1,text="No Audit file selected.", bg='#1F1F1F', fg='white', font = 30, height= 2, width = 78,relief="ridge")
+lbl1.pack(side=LEFT, padx=5, pady=20)
+
+button2 = Button(frame1, text="Browse", command=browse_button, relief="groove")
+button2['font'] = font.Font(size = 10)
+button2.config(height=2, width = 10)
+button2.pack(side=LEFT)
+
+button3 = Button(text="Submit", command=run_prog, bg='#1F1F1F', fg='white', activebackground='#FF8000', relief="raised") # put your function name in the command option maitra
+button3['font'] = myFont
+button3.config(height=1, width = 15)
+button3.pack(side=LEFT, padx=10)
+
+button4 = Button(text="Open Master CSV", command=open_file, bg='#1F1F1F', fg='white', activebackground='#FF8000', relief="raised") # put your function name in the command option maitra
+button4['font'] = myFont
+button4.config(height=1, width = 17)
+button4.pack(side=LEFT, padx=5)
+
+mainloop()
